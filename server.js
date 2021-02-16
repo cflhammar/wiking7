e8237ef7aa66c95cc675cc0a675c4443bd98073c
cflhammar <cflhammar@gmail.com>
2021-02-16 21:13:57 +0100

    server2

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-console.log('server stared PROCFILE');
+const express = require("express");
+const app = express();
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
