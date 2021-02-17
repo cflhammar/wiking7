7b3a5dcea430ec8851629624721cf7b04ec60162
cflhammar <cflhammar@gmail.com>
2021-02-17 19:25:38 +0100

    rebuild fullstack struc2

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const errorhandler = require('errorhandler');
 const express = require('express');
 const morgan = require('morgan');
 const apiRouter = require('./api.js');
+const path = require('path');
 
 const app = express();
-
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(errorhandler());
 app.use(morgan('dev'));
-app.use(express.static('react-ui/build'));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build/index.html')));
 
 app.use('/api', apiRouter);
 app.get('/apa', (req, res, next) => {
