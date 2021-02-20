44748e3f1a3859c081ab2826830c727cd66bbfbd
cflhammar <cflhammar@gmail.com>
2021-02-20 18:50:46 +0100

    deploy

---
+++ 2react-ui/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+
+
+
+
+ReactDOM.render(<div className='all'><div className='Fullpage'> <App /> </div></div>,
+  document.getElementById('root')
+);
+
