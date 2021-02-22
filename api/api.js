cbebb92da4f6a5b99d482206aa0df091fa020879
cflhammar <cflhammar@gmail.com>
2021-02-22 20:41:58 +0100

    deploy

--- api/api.js
+++ api/api.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
-const apiRouter = express.Router();
+const { Pool } = require('pg');
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const apiRouter = express.Router();
 apiRouter.get('/', (req, res, next) => {
     res.status(200).json({ hejsan: 'hejsan' })
 });
 
 const highScoreRouter = require('./highScore.js');
 apiRouter.use('/highScore', highScoreRouter);
+
+apiRouter.get('/pg', async (req, res, next) => {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    console.log("success!")
+})
 
 
 module.exports = apiRouter;
